0c33591f9804932c896ea4d1556d915ec0b9a6e1
No767 <73260931+No767@users.noreply.github.com>
2022-07-10 18:57:48 -0700

    more attempted adjustments

---
+++ src/main/java/io/github/main/transFlagBlock.java
@@ -0,0 +1,10 @@
+package io.github.main;
+
+import net.minecraft.block.Block;
+
+public class transFlagBlock extends Block {
+    public transFlagBlock(Settings settings) {
+        super(settings);
+    }
+    
+}
--- src/main/java/io/github/main/transcraft.java
+++ src/main/java/io/github/main/transcraft.java
@@ -3,7 +3,11 @@ package io.github.main;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -17,10 +21,13 @@ public class transcraft implements ModInitializer {
             () -> new ItemStack(Blocks.COBBLESTONE));
 
     public static final transFlag FABRIC_ITEM = new transFlag(new FabricItemSettings().group(transcraft.transcraftItemGroup));
+    public static final transFlagBlock Trans_Flag_Block = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 
 
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("transcraft", "fabric_item"), FABRIC_ITEM);
+        Registry.register(Registry.BLOCK, new Identifier("transcraft", "transFlagBlock"), new BlockItem(Trans_Flag_Block, new FabricItemSettings().group(transcraft.transcraftItemGroup)));
     }
 }
+
